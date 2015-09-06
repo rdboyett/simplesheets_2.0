7b2a8dcbe4e5cb6d14f1cfacf460d7f15963ddc0
rboyett <dallas20>
2015-09-05 21:14:02 -0500

    fixed table rows

--- userInfo_profile/templatetags/userInfo_tags.py
+++ userInfo_profile/templatetags/userInfo_tags.py
@@ -120,7 +120,10 @@ def questionAverage(formInput):
     
     
     
-    
+@register.simple_tag()
+def multiply(num1, num2, *args, **kwargs):
+    # you would need to do any localization of the result here
+    return num1 * num2
     
     
     
