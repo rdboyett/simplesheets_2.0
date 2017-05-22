e7c37b4accd99ed51fd34f4b7510d7cb95ac97f9
Robert Boyett <rdboyett@gmail.com>
2017-05-21 19:35:48 -0500

    stage settings

--- worksheet_project/settings/stage.py
+++ worksheet_project/settings/stage.py
@@ -2,7 +2,7 @@ from .base import *
 
 ALLOWED_HOSTS = ['qa.ducksoup.us', '*']
 
-STATIC_URL = 'http://ducksoup.us/static/'
+STATIC_URL = 'http://qa.ducksoup.us/static/'
 STATIC_ROOT = '/home/rdboyett/webapps/qa_static/'
 
 SITE_URL="http://qa.ducksoup.us/"
